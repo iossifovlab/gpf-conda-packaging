72838aa80ce0d65dc1bfe7b0b38fb5192f2c36b5
Hristo Stefanov <hrstefanov@yahoo.co.uk>
2025-08-08 10:24:51 +0300

    build.sh: revert to sequential package building

--- build.sh
+++ build.sh
@@ -248,39 +248,37 @@ function main() {
     build_run_container ctx:ctx_spliceai_annotator \
       conda mambabuild \
       -c conda-forge -c bioconda -c file:///wd/builds \
-      /wd/conda-recipes/gpf_spliceai_annotator &
+      /wd/conda-recipes/gpf_spliceai_annotator
 
     build_run_container ctx:ctx_rest_client \
       conda mambabuild --numpy ${numpy_version} \
       -c conda-forge -c bioconda -c file:///wd/builds -c iossifovlab \
-      /wd/conda-recipes/gpf_rest_client &
+      /wd/conda-recipes/gpf_rest_client
 
     build_run_container ctx:ctx_impala_storage \
       conda mambabuild --numpy ${numpy_version} \
       -c conda-forge -c bioconda -c file:///wd/builds -c iossifovlab \
-      /wd/conda-recipes/gpf_impala_storage &
+      /wd/conda-recipes/gpf_impala_storage
 
     build_run_container ctx:ctx_impala2_storage \
       conda mambabuild --numpy ${numpy_version} \
       -c conda-forge -c bioconda -c file:///wd/builds -c iossifovlab \
-      /wd/conda-recipes/gpf_impala2_storage &
+      /wd/conda-recipes/gpf_impala2_storage
 
     build_run_container ctx:ctx_vep_annotator \
       conda mambabuild --numpy ${numpy_version} \
       -c conda-forge -c bioconda -c file:///wd/builds -c iossifovlab \
-      /wd/conda-recipes/gpf_vep_annotator &
+      /wd/conda-recipes/gpf_vep_annotator
 
     build_run_container ctx:ctx_gpfjs \
       conda mambabuild --numpy ${numpy_version} \
       -c conda-forge -c bioconda -c iossifovlab \
-      /wd/conda-recipes/gpf_gpfjs &
+      /wd/conda-recipes/gpf_gpfjs
 
     build_run_container ctx:ctx_wdae \
       conda mambabuild --numpy ${numpy_version} \
       -c conda-forge -c bioconda -c file:///wd/builds -c iossifovlab \
-      /wd/conda-recipes/gpf_wdae &
-
-    wait
+      /wd/conda-recipes/gpf_wdae
 
     # Copy conda packages to the builds directory
     build_run_container ctx:ctx_spliceai_annotator \
